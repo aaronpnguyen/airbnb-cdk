c9ee8f18662d201a25121f43409dab4d077d4faf
aaronpnguyen <aaronnguyen1123@gmail.com>
2023-01-16 10:04:35 -0600

    glue and bucket deployment

--- airbnb_cdk/airbnb_cdk_stack.py
+++ airbnb_cdk/airbnb_cdk_stack.py
@@ -1,19 +1,44 @@
+import aws_cdk as cdk
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    aws_athena,
+    aws_cloudwatch,
+    aws_cloudwatch_actions,
+    aws_events,
+    aws_glue,
+    aws_glue_alpha,
+    aws_s3,
+    aws_s3_deployment,
+    aws_iam as iam
 )
 from constructs import Construct
 
-class AirbnbCdkStack(Stack):
+class AirbnbCdkStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AirbnbCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        ####################################
+        #              Glue!               #
+        ####################################
+
+        self.glue_db = aws_glue_alpha.Database(
+            self,
+            "nguyen-airbnb-deployment", # Id as a string
+            database_name = "nguyen-airnb-database" # Name
+        )
+
+        ####################################
+        #             Buckets!             #
+        ####################################
+
+        s3_client = aws_s3.Bucket(self, "airnb-data-bucket")
+
+        # Adding resource to bucket
+        aws_s3_deployment.BucketDeployment(
+            self,
+            "airbnb-deployment", # Name of bucket
+            sources = [aws_s3_deployment.Source.asset("resources")], # Must be zip/directory
+            destination_bucket = s3_client # Bucket we deploy too
+        )
