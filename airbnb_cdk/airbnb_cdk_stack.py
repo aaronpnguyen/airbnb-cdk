ad0b7a9ff941fcf7c69b8fcc4a26dd22f9da812f
aaronpnguyen <aaronnguyen1123@gmail.com>
2023-01-30 17:52:28 -0600

    glue update + lambda setup

--- airbnb_cdk/airbnb_cdk_stack.py
+++ airbnb_cdk/airbnb_cdk_stack.py
@@ -1,9 +1,11 @@
 import aws_cdk as cdk
 from aws_cdk import (
-    aws_glue,
     aws_glue_alpha as glue,
     aws_s3,
     aws_s3_deployment,
+    aws_lambda,
+    aws_dynamodb as dynamodb,
+    aws_iam as iam
 )
 from constructs import Construct
 
@@ -20,6 +22,9 @@ class AirbnbCdkStack(cdk.Stack):
         resource_bucket = aws_s3.Bucket(self, "nguyen-airbnb-resource-bucket")
         data_bucket = aws_s3.Bucket(self, "nguyen-airbnb-data-bucket")
         glue_bucket = aws_s3.Bucket(self, "nguyen-airbnb-glue-bucket")
+
+        resource_url = resource_bucket.s3_url_for_object()
+        data_url = data_bucket.s3_url_for_object()
 
         ####################################
         #          Bucket Resources!       #
@@ -55,6 +60,17 @@ class AirbnbCdkStack(cdk.Stack):
         )
 
         ####################################
+        #              Lambda!             #
+        ####################################
+
+        aws_lambda.Function(self, "nguyen-test-lambda",
+            runtime = aws_lambda.Runtime.PYTHON_3_8,
+            handler = "airbnb-lambda",
+            code = aws_lambda.Code.from_asset("lambdas")
+        )
+
+
+        ####################################
         #              Glue!               #
         ####################################
 
@@ -64,11 +80,63 @@ class AirbnbCdkStack(cdk.Stack):
             database_name = "nguyen-airbnb-db" # Name
         )
 
+        # For now we're assuming full access, but usually we'd want to implement
+        # The least amount of access
         glue.Job(self, "nguyen-airbnb-price-job",
             executable = glue.JobExecutable.python_etl(
                 glue_version = glue.GlueVersion.V3_0,
                 script = glue.Code.from_bucket(glue_bucket, 'airbnb_price_script.py'),
                 python_version = glue.PythonVersion.THREE,
             ),
-            description = "etl? shell? CDK testing"
+            default_arguments = {
+                "--resource_bucket": resource_url,
+                "--data_bucket": data_url
+            },
+            description = "airbnb-etl"
+        )
+
+        """
+        glue_read_actions = [
+            "glue:GetConnection",
+            "glue:GetDatabase",
+            "glue:GetDatabases",
+            "glue:GetTable",
+            "glue:GetTables",
+        ]
+
+        glue_roles = iam.Role(
+            self,
+            "nguyen-airbnb-etl-role",
+            role_name = "nguyen-airbnb-etl",
+            assumed_by = iam.ServicePrincipal("glue.amazonaws.com"),
+            inline_policies = {
+                "glue_policy": iam.PolicyDocument(
+                    statements = [
+                        iam.PolicyStatement(
+                            actions = [
+                                *glue_read_actions,
+                                "glue:CreateTable",
+                                "glue:DeleteTable"
+                            ],
+                            resources = [self.glue_db.catalog_arn, self.glue_db.database_arn]
+                        )
+                    ]
+                )
+            }
+        )
+        """
+
+        ####################################
+        #            DynamoDb!             #
+        ####################################
+
+        test_table = dynamodb.Table(
+            self,
+            "nguyen-test-table",
+            billing_mode = dynamodb.BillingMode.PAY_PER_REQUEST,
+            point_in_time_recovery = True,
+            removal_policy = cdk.RemovalPolicy.DESTROY,
+            time_to_live_attribute = "ttl",
+            partition_key = dynamodb.Attribute(name = "id", type = dynamodb.AttributeType.STRING),
+            sort_key = dynamodb.Attribute(name = "second_key", type = dynamodb.AttributeType.STRING)
         )
