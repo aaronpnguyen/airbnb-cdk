c9cf10e53300748f2e08321ab7137518ffc5bbd1
aaronpnguyen <aaronnguyen1123@gmail.com>
2023-01-30 10:51:02 -0600

    script re-make

--- src/airbnb_price_script.py
+++ src/airbnb_price_script.py
@@ -5,46 +5,43 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-def main():
-    """""
-    This glue script utilizes 3 nodes and 2 buckets.
-    The first bucket is our resource, where we grab the raw data.
-    
-    When data is grabbed from the first node, we send it to the 2nd node.
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
 
-    The 2nd node transforms our data to then be sent off to the third node
-    which holds the scond bucket.
-    
-    """""
+# Script generated for node S3 bucket
+S3bucket_node1 = glueContext.create_dynamic_frame.from_catalog(
+    database="nguyen-airbnb-db",
+    table_name="area-prices",
+    transformation_ctx="S3bucket_node1",
+)
 
-    # Temp var
-    resource_bucket = "s3://nguyen-airbnb-stack-nguyenairbnbresourcebucket398-7r28e7ydn9g0"
-    data_bucket = "s3://nguyen-airbnb-stack-nguyenairbnbdatabucket5f319e0-8a4lg64btd1a"
+# Script generated for node ApplyMapping
+ApplyMapping_node2 = ApplyMapping.apply(
+    frame=S3bucket_node1,
+    mappings=[
+        ("listing_id", "long", "listing_id", "long"),
+        ("price", "string", "price", "string"),
+        ("nbhood_full", "string", "nbhood_full", "string"),
+    ],
+    transformation_ctx="ApplyMapping_node2",
+)
 
-    # Context build/setup
-    spark_context = SparkContext()
-    logger = GlueContext(spark_context).get_logger()
-    glue_context = GlueContext(spark_context.getOrCreate())
-
-    logger.info("Starting job!")
-
-    raw_dataframe = glue_context.create_dynamic_frame_from_options(
-        connection_type="s3",
-        connection_options={"paths": [resource_bucket]},
-        format="csv",
-        format_options={
-            "withHeader": True
-        }
-    )
-
-    glue_context.write_dynamic_frame_from_options(
-        frame = raw_dataframe,
-        connection_type = "s3",
-        connection_options={"paths": [data_bucket]},
-        format = "parquet"
-    )
-
-    logger.info("Finshed job!")
-
-if __name__ == "__main__":
-    main()
+# Script generated for node S3 bucket
+S3bucket_node3 = glueContext.getSink(
+    path="s3://nguyen-airbnb-stack-nguyenairbnbdatabucket5f319e0-8a4lg64btd1a",
+    connection_type="s3",
+    updateBehavior="UPDATE_IN_DATABASE",
+    partitionKeys=[],
+    enableUpdateCatalog=True,
+    transformation_ctx="S3bucket_node3",
+)
+S3bucket_node3.setCatalogInfo(
+    catalogDatabase="nguyen-airbnb-db", catalogTableName="airbnb-pricing"
+)
+S3bucket_node3.setFormat("glueparquet")
+S3bucket_node3.writeFrame(ApplyMapping_node2)
+job.commit()
