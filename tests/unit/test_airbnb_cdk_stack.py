62c9c7fde1be97510a9554636b4bb7916a7fbf2f
Aaron Nguyen <104883016+aaronpnguyen@users.noreply.github.com>
2023-02-22 16:11:04 -0600

    Lambda tests (#11)
    * Lambda tests
    
    * Added environment variable for dynamodb patch

--- tests/unit/test_airbnb_cdk_stack.py
+++
@@ -1,9 +0,0 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
-
-from airbnb_cdk.airbnb_cdk_stack import AirbnbCdkStack
-
-# example tests. To run these tests, uncomment this file along with the example
-# resource in airbnb_cdk/airbnb_cdk_stack.py
-def test_workflow():
-    assert 1 == 1
